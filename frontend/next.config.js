521c4fd1f818489832b30190135c6474c14f60dd
Mohamed Danis <62685219+MohamedDanis@users.noreply.github.com>
2023-11-11 20:46:05 +0530

    fixed cors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         return [
           {
             // matching all API routes
-            source: "https://mohameddanis.live:path*",
+            source: "https://mohameddanis.live/:path*",
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
               { key: "Access-Control-Allow-Origin", value: "*" },
